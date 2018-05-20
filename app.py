2514c0cab1ad244287bde296362a6ca09d0035a7
lanian2219 <13756332+lanian2219@users.noreply.github.com>
2018-05-20 02:48:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.after_request
-def set_response_headers(response):
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
-
 @app.route('/')
 def login():
     return render_template("index.html")
